e65e39086a4bc1c9cbe64e770a54e45d400440cc
Vinay <vinay00mahto@gmail.com>
2026-06-09 21:18:34 +0530

    add

--- app/api/users/verifymail/route.ts
+++ app/api/users/verifymail/route.ts
@@ -1,10 +1,9 @@
-import {connect} from "@/dbConfig/dbConfig";
+import dbConnect from "@/app/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 
 
-
-connect()
+await dbConnect();
 
 
 export async function POST(request: NextRequest){
@@ -21,7 +20,7 @@ export async function POST(request: NextRequest){
         }
         console.log(user);
 
-        user.isVerfied = true;
+        user.isVerified = true;
         user.verifyToken = undefined;
         user.verifyTokenExpiry = undefined;
         await user.save();
